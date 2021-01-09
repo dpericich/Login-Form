5eadd3c6930fadc851ce6481e31a348b13ab5dc3
Daniel <dpericich@gmail.com>
2021-01-09 11:55:43 -0600

    base setup and sass support files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
+import Input from "./components/Input";
+import Button from "./components/Button";
+import PopUp from "./components/PopUp";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    username: null,
+    password: null,
+    popUpOn: false,
+  };
+
+  getUserName = (val) => {
+    this.setState({ username: val });
+  };
+
+  getPassword = (val) => {
+    this.setState({ password: val });
+  };
+
+  togglePopUP = () => {
+    this.setState({ popUpOn: !this.state.popUpOn });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="login">
+          <div className="login__header">
+            <h1 className="login__header--title">ShoeLocker</h1>
+          </div>
+          <Input
+            placeholder="Email / Username"
+            text={this.state.username}
+            getInput={this.getUserName}
+          />
+          <Input
+            placeholder="Password"
+            text={this.state.password}
+            getInput={this.getPassword}
+          />
+          <Button name="Log In" />
+          <a href="#" className="link" onClick={() => this.togglePopUP()}>
+            Click Here to Create Account &rarr;
+          </a>
+        </div>
+        {this.state.popUpOn ? (
+          <PopUp title={"Create Account"} action={this.togglePopUP} />
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default App;
